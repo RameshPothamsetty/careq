3e2f289dbff1a5c0845bbe998f3c94e576ca71d5
RameshPothamsetty <rap53748@gmail.com>
2026-07-29 11:17:19 +0530

    fix: add io.jsonwebtoken.Claims import to JwtAuthGlobalFilter

--- backend/api-gateway/src/main/java/com/careq/gateway/filter/JwtAuthGlobalFilter.java
+++ backend/api-gateway/src/main/java/com/careq/gateway/filter/JwtAuthGlobalFilter.java
@@ -1,5 +1,6 @@
 package com.careq.gateway.filter;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
